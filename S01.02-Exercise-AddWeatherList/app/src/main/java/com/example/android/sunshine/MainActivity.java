ba5634e954a7d12731fe76f594ec5d24bba8e84a
mregtej <mregtej@gmail.com>
2017-11-07 15:15:02 +0100

    Completed S01.02-Exercise-AddWeatherList

--- S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -17,20 +17,36 @@ package com.example.android.sunshine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (1) Create a field to store the weather display TextView
+    // DONE (1) Create a field to store the weather display TextView
+    private TextView mWeatherTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
 
-        // TODO (2) Use findViewById to get a reference to the weather display TextView
+        // DONE (2) Use findViewById to get a reference to the weather display TextView
+        mWeatherTextView = (TextView) findViewById(R.id.tv_weather_data);
 
-        // TODO (3) Create an array of Strings that contain fake weather data
+        // DONE (3) Create an array of Strings that contain fake weather data
+        String[] fakeWeatherData = {
+                "Tue, 7th of November, 2017 - Sunny - 12°C / 20°C",
+                "Wed, 8th of November, 2017 - Cloudy - 10°C / 14°C",
+                "Thu, 9th of November, 2017 - Rainy - 8°C / 12°C",
+                "Fri, 10th of November, 2017 - Snowy - 0°C / 2°C",
+                "Sat, 11th of November, 2017 - Foggy - 4°C / 8°C",
+                "Sun, 12th of November, 2017 - Sunny - 18°C / 27°C",
+                "Mon, 13th of November, 2017 - Stormy - 14°C / 21°C",
+                "Tue, 14th of November, 2017 - Rainy - 12°C / 20°C"
+        };
 
-        // TODO (4) Append each String from the fake weather data array to the TextView
+        // DONE (4) Append each String from the fake weather data array to the TextView
+        for(String fakeWeatherEntry : fakeWeatherData) {
+            mWeatherTextView.append(fakeWeatherEntry + "\n\n");
+        }
     }
 }
